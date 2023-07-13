92c44b083e371b4acadd10f0af1f5e18709b6782
AntonHran <anton.hranovskyi@gmail.com>
2023-07-13 11:41:54 +0300

    First commit

---
+++ cleaner_functions.py
@@ -0,0 +1,225 @@
+import asyncio
+import time
+import os
+import re
+import unicodedata
+
+import aiofiles
+import aiofiles.os
+import aioshutil
+from aiopath import AsyncPath
+
+from cleaner_consts import table
+from typing import Tuple, Callable
+from classes import TerminalView
+from logger import get_logging
+
+logger_ = get_logging(__name__)
+
+cleaner_commands = TerminalView()
+cleaner_commands_comm: str = '''
+    To sort files, type the path to your folder according the pattern: <DISC:\\Folder\\Other folder...>
+    To back to main menu, type: <back>
+    To see instructions of this module, type: <help>'''
+cleaner_commands.add_command(cleaner_commands_comm)
+root: str = ''
+
+
+async def process_directory(directory: str) -> None:
+    logger_.info(f'Function process_directory: {directory}')
+    async for path in AsyncPath(directory).iterdir():
+        if await path.is_file():
+            await process_file(directory, path.name)
+        elif await path.is_dir() and path.name.lower() not in extensions.keys():
+            await process_directory(path)
+
+    '''for file in os.listdir(directory):
+        file_path: str = os.path.join(directory, file)
+        if await aiofiles.os.path.isfile(file_path):
+            await process_file(directory, file)
+        elif await aiofiles.os.path.isdir(file_path) and file.lower() not in extensions.keys():
+            await process_directory(file_path)'''
+
+
+async def process_file(file_path: str, file: str) -> None:
+    logger_.info(f'Function process_file: {file} from {file_path}')
+    name, extension = file.rsplit('.', 1)
+    func_: tuple = handle_func(extension)
+    await func_[0](file_path, f'{root}\\{func_[1]}', name, extension)
+    await check_folder(file_path)
+    await asyncio.sleep(0.001)
+
+
+async def deal_with_copies(old_path: str, new_path: str, new_name: str, ext: str) -> None:
+    i: int = 0
+    while True:
+        if f'{new_name}.{ext}' not in os.listdir(new_path):
+            await aiofiles.os.replace(f'{old_path}\\{new_name}.{ext}', f'{new_path}\\{new_name}.{ext}')
+            break
+        else:
+            i += 1
+            file_name: str = new_name
+            new_name = f'{file_name}_{i}'
+            await aiofiles.os.rename(f'{old_path}\\{file_name}.{ext}', f'{old_path}\\{new_name}.{ext}')
+
+
+async def move_to(old_path: str, new_path: str, file_name: str, ext: str) -> None:
+    logger_.info(f'Function move_to: {file_name} from {old_path}')
+    new_name: str = rename(file_name)
+    await aiofiles.os.rename(f'{old_path}\\{file_name}.{ext}', f'{old_path}\\{new_name}.{ext}')
+    await deal_with_copies(old_path, new_path, new_name, ext)
+
+
+async def move_to_archive(old_path: str, new_path: str, file_name: str, ext: str) -> None:
+    logger_.info(f'Function move_to_archive: {file_name} from {old_path}')
+    new_name: str = rename(file_name)
+    try:
+        await aiofiles.os.makedirs('\\'.join((new_path, new_name.title())))
+        await aioshutil.unpack_archive(f'{old_path}\\{file_name}.{ext}', '\\'.join((new_path, new_name.title())))
+        await aiofiles.os.remove(f'{old_path}\\{file_name}.{ext}')
+    except FileExistsError:
+        await aiofiles.os.remove(f'{old_path}\\{file_name}.{ext}')
+
+
+'''async def move_to_other(old_path: str, new_path: str, file_name: str, ext: str) -> None:
+    logger_.info(f'Function move_to_other: {file_name}.{ext} from {old_path}')
+    new_name: str = rename(file_name)
+    await aiofiles.os.rename(f'{old_path}\\{file_name}.{ext}', f'{old_path}\\{new_name}.{ext}')
+    await deal_with_copies(old_path, new_path, new_name, ext)'''
+
+
+def rename(file_name: str) -> str:
+    logger_.info(f'Function rename: {file_name}')
+    pattern = r'[a-zA-Z0-9\.\-\(\)]'
+    for char in file_name:
+        if not re.match(pattern, char):
+            if ord(unicodedata.normalize('NFC', char.lower())) in table.keys():
+                file_name = file_name.replace(char, char.translate(table))
+            else:
+                file_name = file_name.replace(char, '_')
+    return file_name
+
+
+async def check_folder(old_path: str) -> None:
+    logger_.info(f'Function check_folder: {old_path}')
+    if not os.listdir(old_path):
+        await aiofiles.os.rmdir(old_path)
+
+
+extensions = dict(images=[('jpeg', 'png', 'jpg', 'svg'), move_to],
+                  video=[('avi', 'mp4', 'mov', 'mkv'), move_to],
+                  documents=[('doc', 'docx', 'txt', 'pdf', 'xlsx', 'pptx'), move_to],
+                  audio=[('mp3', 'ogg', 'wav', 'amr', 'm4a'), move_to],
+                  web=[('html', 'xml', 'csv', 'json'), move_to],
+                  archive=[('zip', 'gz', 'tar'), move_to_archive])
+
+
+def handle_func(file_extension: str) -> Tuple[Callable, str]:
+    logger_.info(f'Function handle_func: {file_extension}')
+    for category, extension in extensions.items():
+        if file_extension in extension[0]:
+            return extension[1], category.title()
+    return move_to, 'Other'
+
+
+async def after_check(path: str) -> None:
+    logger_.info('Sorting was finished. After sorting checking is started')
+    for folder in os.listdir(path):
+        if get_folder_size(f'{path}\\{folder}') == 0:
+            await aioshutil.rmtree(f'{path}\\{folder}')
+
+
+def get_folder_size(folder_path: str) -> int:
+    logger_.info('getting folder size')
+    total_size: int = 0
+    for roots, dirs, files in os.walk(folder_path):
+        for file in files:
+            file_path = os.path.join(roots, file)
+            total_size += os.path.getsize(file_path)
+    return total_size
+
+
+async def make_directories(path: str) -> None:
+    logger_.info('Function make_directories')
+    for key in extensions:
+        await aiofiles.os.makedirs('\\'.join((path, key.title())), exist_ok=True)
+    await aiofiles.os.makedirs('\\'.join((path, 'Other')), exist_ok=True)
+
+
+async def check_path(path: str) -> str | None:
+    logger_.info(f'Function check_path: {path}')
+    if await aiofiles.os.path.exists(path):
+        return path
+    else:
+        logger_.error(f'Error with an entered path: {path}')
+        print('Something went wrong. Check a validity of the entered path.')
+
+
+def instructions() -> None:
+    logger_.info('Function instructions')
+    for command in cleaner_commands.display_commands():
+        print(command)
+
+
+async def clean_folder(path: str):
+    checked_path: str = await check_path(path)
+    if checked_path:
+        global root
+        root = checked_path
+        await make_directories(path)
+        await process_directory(path)
+        await after_check(path)
+        print(f'The folder in path {checked_path} was cleaned.')
+
+
+functions = {'help': instructions, 'path': clean_folder}
+
+
+async def handler(command: str):
+    logger_.debug(f'handling command: {command}')
+    pattern = re.compile(r'([A-Z]:\\){1}(\w+\\)*(\w+){1}')
+    if re.match(pattern, command):
+        return await functions['path'](command)
+    try:
+        return functions[command]()
+    except KeyError as error:
+        logger_.error(f'Error occurred: {error}')
+        print('I do not understand what you want to do. Please look at instructions, type <help>')
+
+
+def greeting_cleaner() -> None:
+    logger_.info('Function greeting_cleaner')
+    print('''\n\tNow you are in the cleaning module. In this module I help you to sort all files 
+    in the chosen directory thus cleaning your folder.''')
+
+
+async def clean_folder_main() -> None:
+    logger_.info('Function clean_folder_main')
+    greeting_cleaner()
+    instructions()
+    while True:
+        answer: str = input('\nType a path to a folder to clean or a command: ')
+        if answer == 'back':
+            print('\nYou returned to the main Menu.')
+            break
+        start = time.time()
+        await handler(answer)
+        finish = time.time()
+        print(finish - start)
+# Without handler() and dict functions:
+
+'''async def clean_folder_main() -> None:
+    logger_.info('Function clean_folder_main')
+    greeting_cleaner()
+    instructions()
+    while True:
+        answer: str = input('\nType a path to a folder to clean or a command: ')
+        if answer == 'back':
+            print('\nYou returned to the main Menu.')
+            break
+        elif answer == 'help':
+            instructions()
+        else:
+            await clean_folder(answer)'''
+
+# asyncio.run(clean_folder_main())
---
+++ game.py
@@ -0,0 +1,138 @@
+import random
+import re
+from typing import Callable
+
+import requests
+
+from illustration_to_game import draw
+from classes import TerminalView
+from logger import get_logging
+
+
+logger_ = get_logging(__name__)
+
+game_commands = TerminalView()
+game_commands_comm = '''
+    Type "play" to play the game, "score" to show the scoreboard, "help" to show instructions
+    and "back" to return to main menu.'''
+game_commands.add_command(game_commands_comm)
+
+
+'''def choose() -> tuple:
+    word_list: list = ['python', 'java', 'swift', 'javascript', 'ruby', 'html', 'php']
+    word: str = random.choice(word_list)
+    gues_word: str = '-' * len(word)
+    return word, gues_word'''
+
+
+def get_words_list() -> list:
+    logger_.info('Function get_words_list: creating list of words')
+    response = requests.get('https://www.mit.edu/~ecprice/wordlist.10000', timeout=10)
+    string_words: str = response.content.decode('utf-8')
+    list_words: list = string_words.splitlines()
+    return list_words
+
+
+words: list = get_words_list()
+score = dict(win=0, loose=0)
+
+
+def game() -> None:  # -> tuple:
+    # word, gues_word = choose()
+    logger_.info('Start the game!')
+    word: str = random.choice(words)
+    gues_word: str = '-' * len(word)
+    i: int = 0
+    entered_letters: str = ''
+    while i < 8 and gues_word != word:
+        print('\n' + gues_word)
+        letter: str = input('Input a letter: ')
+        letter = check_enter_by_error(letter)
+        if letter:
+            i_res, gues_word = check_letter(entered_letters, gues_word, letter, word, i)
+            i = i_res
+    result(word, gues_word)
+
+
+def check_letter(entered_letters: str, gues_word: str, letter: str, word: str, i: int) -> tuple:
+    logger_.info('checking entered letter')
+    entered_letters += letter
+    if entered_letters.count(letter) > 1:
+        print("You have already guessed this letter.")
+    elif letter in word:
+        gues_word = show_changes(word, letter, gues_word)
+    else:
+        print(f"The letter {letter} doesn't present in the word.")
+        i += 1
+        draw(i)
+    return i, gues_word
+
+
+def result(word: str, guested_word: str):  # -> tuple:
+    logger_.info('Function result')
+    if word != guested_word:
+        print(f'\nYou lost! The word was {word}')
+        score['loose'] += 1
+    else:
+        print(f'\nYou guessed the word: {word}! \nYou survived!')
+        score['win'] += 1
+
+
+def show_changes(wrd: str, let: str, str_: str) -> str:
+    logger_.info('Function show_changes')
+    str_: list = list(str_)
+    for i in range(len(wrd)):
+        if wrd[i] == let:
+            str_[i] = wrd[i]
+    return ''.join(str_)
+
+
+def check_enter_by_error(let: str) -> str:
+    logger_.info(f'checking enter values: {let}')
+    pattern = re.compile(r'[a-z]')
+    matches = re.search(pattern, let)
+    if matches:
+        return let
+    elif len(let) == 1 and (let.isupper() or let.isascii()):
+        print('Please, enter a lowercase letter from the English alphabet.')
+    else:
+        print('Please, input a single letter.')
+
+
+def instructions_game() -> None:
+    logger_.info('Function instructions_game')
+    for command in game_commands.display_commands():
+        print(command)
+
+
+def greeting_game() -> None:
+    print('''\n\tH A N G M A N
+        This is entertainment module with a well known game - hangman. So are you ready to gues a word?''')
+
+
+def show_score() -> None:
+    logger_.info('Function show_score')
+    print(f'\nYou won: {score["win"]} times.\nYou lost: {score["loose"]} times.')
+
+
+functions = {'play': game, 'score': show_score, 'help': instructions_game}
+
+
+def handler(command: str) -> Callable:
+    try:
+        return functions[command]()
+    except KeyError as error:
+        logger_.error(f'Error was occurred: {error}')
+        print('I do not understand what you want. Please look at instructions.')
+
+
+def game_main():
+    logger_.info('game module is started')
+    greeting_game()
+    instructions_game()
+    while True:
+        answer = input('\nMake your choice: ')
+        if answer == 'back':
+            print('\nYou returned to the main Menu.')
+            break
+        handler(answer)
